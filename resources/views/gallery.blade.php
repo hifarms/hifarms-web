86f913ebca2e2ba48d35dfc046f4c9c6a8826e4d
Muhammad Sani Haruna <muhammadsaniharuna44@gmail.com>
2022-05-22 18:07:21 +0100

    fixes

--- resources/views/gallery.blade.php
+++ resources/views/gallery.blade.php
@@ -10,50 +10,25 @@
 <div class="about-description">
 <a href="index.html">Home</a> <img src="img/Arrow 1.svg" width="12"> <a href="about.html">About</a> <img src="img/Arrow 1.svg" width="12"> <a href="gallery.html">Gallery</a>
 </div>
- <!--
-      <div class="gallery gallery-container">
-        <div class="gallery-grid">
-           <img src="img/Rectangle 23.png" alt="image" class="grid">
-            <img src="img/grassfield@4x 1.png" alt="image" class="grid">
-            <img src="img/unsplash_Bbq3H7eGids.png" alt="image" class="grid" >
-            <img src="img/unsplash_fQ9X2rPEwq8.png" alt="image" class="grid">
-            <img src="img/unsplash_CAA5XShS_No.png" alt="image" class="grid">
-            <img src="img/unsplash_s0lkbvtN9K8.png" alt="image" class="grid">
-        </div>
-    </div>
- -->
-
-
-
-
-
-
-
-
-
-
-
-
-
 <!-- Images used to open the lightbox -->
 <div class="row">
     <div class="column">
-      <img src="img/grassfield@4x 1.png" onclick="openModal();currentSlide(1)" class="hover-shadow">
+      <img src="img/Rectangle 23.png" onclick="openModal();currentSlide(1)" class="hover-shadow">
     </div>
     <div class="column">
-      <img src="img/unsplash_fQ9X2rPEwq8.png" onclick="openModal();currentSlide(2)" class="hover-shadow">
+      <img src="img/grassfield@4x 1.png" onclick="openModal();currentSlide(2)" class="hover-shadow">
     </div>
     <div class="column">
-      <img src="img/Rectangle 23.png" onclick="openModal();currentSlide(3)" class="hover-shadow">
+      <img src="img/unsplash_Bbq3H7eGids.png" onclick="openModal();currentSlide(3)" class="hover-shadow">
     </div>
     <div class="column">
-      <img src="img/grassfield@4x 1.png" onclick="openModal();currentSlide(4)" class="hover-shadow">
+      <img src="img/unsplash_fQ9X2rPEwq8.png" onclick="openModal();currentSlide(4)" class="hover-shadow">
     </div>
     <div class="column">
-        <img src="img/grassfield@4x 1.png" onclick="openModal();currentSlide(4)" class="hover-shadow">
+        <img src="img/unsplash_CAA5XShS_No.png" onclick="openModal();currentSlide(4)" class="hover-shadow">
       </div>
       <div class="column">
-        <img src="img/grassfield@4x 1.png" onclick="openModal();currentSlide(4)" class="hover-shadow">
+        <img src="img/unsplash_s0lkbvtN9K8.png" onclick="openModal();currentSlide(4)" class="hover-shadow">
       </div>
   </div>
   
@@ -64,32 +39,32 @@
   
       <div class="mySlides">
         <div class="numbertext">1 / 6</div>
-        <img src="img/grassfield@4x 1.png">
-      </div>
-  
-      <div class="mySlides">
-        <div class="numbertext">2 / 6</div>
-        <img src="img/unsplash_fQ9X2rPEwq8.png">
-      </div>
-  
-      <div class="mySlides">
-        <div class="numbertext">3 / 6</div>
         <img src="img/Rectangle 23.png">
       </div>
   
       <div class="mySlides">
-        <div class="numbertext">4 / 6</div>
+        <div class="numbertext">2 / 6</div>
         <img src="img/grassfield@4x 1.png">
+      </div>
+  
+      <div class="mySlides">
+        <div class="numbertext">3 / 6</div>
+        <img src="img/unsplash_Bbq3H7eGids.png">
+      </div>
+  
+      <div class="mySlides">
+        <div class="numbertext">4 / 6</div>
+        <img src="img/unsplash_fQ9X2rPEwq8.png">
       </div>
 
       <div class="mySlides">
         <div class="numbertext">5 / 6</div>
-        <img src="img/grassfield@4x 1.png">
+        <img src="img/unsplash_CAA5XShS_No.png">
       </div>
 
       <div class="mySlides">
         <div class="numbertext">6 / 6</div>
-        <img src="img/grassfield@4x 1.png">
+        <img src="img/unsplash_s0lkbvtN9K8.png">
       </div>
   
       <!-- Next/previous controls -->
