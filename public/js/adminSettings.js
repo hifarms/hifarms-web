8a37b3f0fde17765dd585365314cba86dc73ff15
Muhammad Sani Haruna <muhammadsaniharuna44@gmail.com>
2022-07-14 07:25:32 +0100

    Admin Settings

--- public/js/adminSettings.js
+++ public/js/adminSettings.js
@@ -121,3 +121,56 @@ changePassword.addEventListener('click', (e)=>{
     e.preventDefault();
     showPasswordModal();
 })
+
+const editLoader = document.querySelector('.edit-want-to-sell-span');
+
+const addEditLoader = (e)=>{
+    e.preventDefault();
+
+    let submitButton = document.querySelector('.edit-want-to-sell-span');
+    let loader = document.querySelector('.loader-edit');
+ 
+    submitButton.style.display = 'none'
+    loader.style.display = 'block';
+    
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+ 
+    setTimeout(()=>{
+       let addItemModal = document.querySelector('.admin-add-item');
+       let overlayEffect = document.querySelector('.overlay');
+       let addedClass = document.querySelector('.added-successfully');
+       let submitButton = document.querySelector('.edit-want-to-sell-span');
+       let loader = document.querySelector('.loader-edit');
+    
+       submitButton.style.display = 'block'
+       loader.style.display = 'none';
+       overlayEffect.style.display = 'none';
+       addItemModal.style.display = 'none';
+       addedClass.style.display = 'block';
+ 
+       setTimeout(()=>{
+          let addedClass = document.querySelector('.added-successfully');
+          addedClass.style.display = 'none'
+       }, 2500)
+       
+    }, 1500)
+ }
+
+ editLoader.addEventListener('click', addEditLoader);
+
+ const saveLoader = document.querySelector('.save-changes');
+
+ saveLoader.addEventListener('click', (e)=>{
+     e.preventDefault()
+     
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+
+    let addedClass = document.querySelector('.save-successful');
+    addedClass.style.display = 'block'
+    
+    setTimeout(()=>{
+        addedClass.style.display = 'none'
+     }, 1500)
+ })
