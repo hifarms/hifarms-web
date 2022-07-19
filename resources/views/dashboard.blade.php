59ba12e5204f6bf31e42a852a0b29c72caf46af5
Muhammad Sani Haruna <muhammadsaniharuna44@gmail.com>
2022-07-19 21:16:47 +0100

    mobile view

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -8,10 +8,44 @@
     <title>Dashboard</title>
 </head>
 <body>
-    <header>
+    <!--Dashboard Hamburger Starts-->
+      <div class="dashboard-hamburger">
+        <div class="dash-hamb-img-name">
+        <img src="img/dashboard-hamburger-profile.png" alt="profile" class="profile-header ham">
+        <h2>Welcome, <br>
+            Hafiz
+        </h2>
+        </div>
+        <div class="input-search ham">
+            <input type="text" placeholder="search for">
+            <img src="img/Vector (8).png" alt="search-icon">
+        </div>
+        <div class="side-ar">
+        <h3>General</h3>
+        <div class="second-se-bar">
+            <a href="dashboard"> <img src="img/Dashboard.svg" alt="dashboard" class="current"> <p>Dashboard</p></a>
+            <a href="marketplace"><img src="img/marketplace.svg" alt="marketplace"> <p>Marketplace</p></a>
+            <a href="user-farms"><img src="img/livestock-dashboard.svg" alt="livestock"> <p>Manage my farm</p></a>
+            <a href="farm-invest"><img src="img/invest.svg" alt="marketplace"> <p>Invest In a Project</p></a>
+            <a href="investment"><img src="img/history.svg" alt="history"> <p>ROI</p></a>
+            <a href="wallet kun"><img src="img/wallet.svg" alt="wallet"> <p>Wallet</p></a>
+        </div>
+    </div>
+    <div class="third-se-bar">
+            <a href="userSettings.html"><img src="img/settings.png" alt="settings"> <p>Settings</p></a>
+            <a href=""><img src="img/log out.svg" alt="settings"> <p>Logout</p></a>
+    </div>
+    <div class="guide-dash">
+        <img src="img/Notifications.png" alt="notifications" class="notifications">
+        <img src="img/Guides.png" alt="guides" width="32">
+    </div>
+      </div>
+    <!--Dashboard Hamburger Ends-->
+    <header class="dashbrd-header">
         <div class="dashboard-header">
+        <img src="img/hamburger.svg" alt="#" id="hamburger" class="hamburger1">
             <div class="header-img">
-                <img src="img/logo.png" alt="logo" width="113">
+                <img src="img/logo.png" class="logo1x" alt="logo" width="113">
             </div>
             <div class="header-icons">
                 <div class="input-search">
@@ -80,9 +114,10 @@
             <img src="img/log out.svg" alt="log-out">
         </div>
     </div>
-    <div class="dashboard-container">
+    <div class="dashboard-container dash">
         <h1>Dashboard.</h1>
         <div class="dashboard-flex">
+        <div class="first">
         <a href="/marketplace">
             <div class="marketplace-flex">
                 <div class="top-marketplace-flex">
@@ -118,6 +153,8 @@
                     </div>
                 </div>
             </div>
+            </div>
+            <div class="second">
             <div class="marketplace-flex manage-your-farm">
                 <a href="user-farms">
                 <div class="top-marketplace-flex">
@@ -147,9 +184,11 @@
                     <img src="img/add-my-farm.svg" alt="eye-icon" width="23">
                 </div>
             </div>
+            </div>
         </div>
         </a>
     </div>
+    <script src="js/dashboardHamburger.js"></script>
     <script src="js/userDashboard.js"></script>
 </body>
 </html>
