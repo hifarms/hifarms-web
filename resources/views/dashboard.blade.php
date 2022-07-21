fe3843e19e86d9544706fd27cc714f8567312959
Muhammad Sani Haruna <muhammadsaniharuna44@gmail.com>
2022-07-21 13:15:19 +0100

    changes

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -117,7 +117,7 @@
     <div class="dashboard-container dash">
         <h1>Dashboard.</h1>
         <div class="dashboard-flex">
-        <div class="first h">
+        <div class="first">
         <a href="/marketplace">
             <div class="marketplace-flex">
                 <div class="top-marketplace-flex">
